d70c4a6da3de18ce5aca99b5e95fefdac23560a1
Niranjana <ponniranjana2006@gmail.com>
2026-06-17 22:09:07 +0530

    Create C_Problem_6.c

--- C_Problem_6.c
+++ C_Problem_6.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-void disp_odd(int);
+void disp_2digit_odd_below20(int);
 int main()
 {
     int x;
     x=19;
-    disp_odd(x);
+    disp_2digit_odd_below20(x);
 }
-void disp_odd(int x)
+void disp_2digit_odd_below20(int x)
 {
     int i;
     for(i=11;i<=x;i=i+2)
