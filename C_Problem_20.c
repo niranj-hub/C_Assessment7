6c5eddbf3ac15d550589a192d4bd46b5d947f469
Niranjana <ponniranjana2006@gmail.com>
2026-06-20 22:33:44 +0530

    Create C_Problem_20.c

---
+++ C_Problem_20.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void disp_2digit_even_sum6(int x);
+int main()
+{
+    int x;
+    x=0;
+    disp_2digit_even_sum6(x);
+    return 0;
+}
+void disp_2digit_even_sum6(int x)
+{
+    x=4;
+    printf("%d",x);
+}
+    
