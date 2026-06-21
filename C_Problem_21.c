7883a4e74ff4d50bd15ce22be3f5b274914fcac2
Niranjana <ponniranjana2006@gmail.com>
2026-06-21 21:39:10 +0530

    Create C_Problem_21.c

---
+++ C_Problem_21.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void disp_total_odd_digits(int x);
+int main()
+{
+    int x;
+    printf("Enter a number:");
+    scanf("%d", &x);
+    disp_total_odd_digits(x);
+}
+void disp_total_odd_digits(int x)
+{
+    int rem,count;
+    count=0;
+    while(x!=0)
+    {
+        rem=x%10;
+        if(rem%2!=0)
+        {
+            count++;
+        }
+        x=x/10;
+    }
+    printf("%d",count);
+}
